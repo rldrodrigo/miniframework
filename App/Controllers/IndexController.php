f8a12a44616fc3be4ca349b8d3dad4262d39c416
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-05 16:38:53 -0300

    Configurando as rotas

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController
+{
+
+    public function index()
+    {
+        echo 'Chegamos ao IndexController e disparamos a action index';
+    }
+
+    public function sobreNos()
+    {
+        echo 'Chegamos ao IndexController e disparamos a action sobreNos';
+    }
+}
+
+?>
+
+<!-- teste -->
---
+++ App/Route.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use MF\Init\Bootstrap;
+
+class Route extends Bootstrap
+{
+    public function initRoutes()
+    {
+        $routes['home'] = array(
+            'route' => '/',
+            'controller' => 'indexController',
+            'action' => 'index'
+        );
+
+        $routes['sobre_nos'] = array(
+            'route' => '/sobre_nos',
+            'controller' => 'indexController',
+            'action' => 'sobreNos'
+        );
+
+        $this->setRoutes($routes);
+    }
+}
