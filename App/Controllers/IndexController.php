d245450c25c0579ce78cd1b9cf0d1990982284ed
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-05 17:29:01 -0300

    Configurando as views

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -5,14 +5,35 @@ namespace App\Controllers;
 class IndexController
 {
 
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new \stdClass();
+    }
+
     public function index()
     {
-        echo 'Chegamos ao IndexController e disparamos a action index';
+        $this->view->dados = array('Sofá', 'Cadeira', 'Cama');
+        $this->render('index');
     }
 
     public function sobreNos()
     {
-        echo 'Chegamos ao IndexController e disparamos a action sobreNos';
+        $this->view->dados = array('Notebook', 'Calular', 'Banana');
+        $this->render('sobreNos');
+    }
+
+    public function render($view)
+    {
+        $classeAtual = get_class($this);
+
+        $classeAtual =  str_replace('App\\Controllers\\', '', $classeAtual);
+
+        $classeAtual = strtolower(str_replace('Controller', '', $classeAtual));
+
+
+        require_once "../App/Views/$classeAtual/$view.phtml";
     }
 }
 
